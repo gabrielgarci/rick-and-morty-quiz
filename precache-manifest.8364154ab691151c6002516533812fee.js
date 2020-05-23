3a9e0895e4cbb15f6d4428950e4d3607f2eec8bf
Gabriel García <garcia.rodriguez.gabriel@gmail.com>
2020-05-23 13:17:52 +0200

    Updates

---
+++ precache-manifest.8364154ab691151c6002516533812fee.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629bdb800558e457862eac0d2449e796",
+    "url": "/rick-and-morty-quiz/index.html"
+  },
+  {
+    "revision": "efe3f07b950cb9cada2d",
+    "url": "/rick-and-morty-quiz/static/css/main.cf0b14a3.chunk.css"
+  },
+  {
+    "revision": "44902a838f4005ddb0f4",
+    "url": "/rick-and-morty-quiz/static/js/2.0f6acd71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-and-morty-quiz/static/js/2.0f6acd71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe3f07b950cb9cada2d",
+    "url": "/rick-and-morty-quiz/static/js/main.b6a02de4.chunk.js"
+  },
+  {
+    "revision": "e0dad865865581857247",
+    "url": "/rick-and-morty-quiz/static/js/runtime-main.c43ec2ea.js"
+  },
+  {
+    "revision": "d6d0b48900b27cad7176bd0229c2c454",
+    "url": "/rick-and-morty-quiz/static/media/arrow.d6d0b489.svg"
+  },
+  {
+    "revision": "82d32ff6fc8a62b96ff95bca7d75dbe6",
+    "url": "/rick-and-morty-quiz/static/media/finish.82d32ff6.png"
+  },
+  {
+    "revision": "15fc282ef82f9fcfca52e7391cd62368",
+    "url": "/rick-and-morty-quiz/static/media/fire.15fc282e.png"
+  },
+  {
+    "revision": "842f63c30ea08a7817170cad48d5ebfb",
+    "url": "/rick-and-morty-quiz/static/media/logo.842f63c3.png"
+  },
+  {
+    "revision": "617393a99e351f05c276a18cfad9a834",
+    "url": "/rick-and-morty-quiz/static/media/morty-face.617393a9.png"
+  },
+  {
+    "revision": "bb65bdad3149a58a62a10b9d53c9c020",
+    "url": "/rick-and-morty-quiz/static/media/record.bb65bdad.png"
+  },
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "/rick-and-morty-quiz/static/media/title.42d6efdb.png"
+  }
+]);
