15c6b281efd7e6943d1c794cde3944757faa0f61
Gabriel García Rodríguez <ggarciaro@minsait.com>
2020-07-29 13:32:07 +0200

    Fix gh pages refresh bug

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 
 import './App.scss'
 import HomePage from './HomePage/HomePage'
@@ -7,14 +7,17 @@ import Game from './Game/Game'
 import Records from './Records/Records'
 
 const App = () => {
+
   return (
-    <div className="app">
-      <Switch>
-        <Route path="/game" component={ Game } />
-        <Route path="/records" component={ Records } />
-        <Route path="/" component={ HomePage } />
-      </Switch>
-    </div>
+    <HashRouter basename='/'>
+      <div className="app">
+        <Switch>
+          <Route path="/game" component={ Game } />
+          <Route path="/records" component={ Records } />
+          <Route path="/" component={ HomePage } />
+        </Switch>
+      </div>
+    </HashRouter>
   )
 }
 
--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './HomePage.scss'
 import title from '../../assets/images/title.png'
 import logo from '../../assets/images/logo.png'
 import Button from '../../shared/Button/Button'
-import { Link } from 'react-router-dom'
 
 const HomePage = () => {
+
     return (
         <div className="menu">
             <img className="menu__title" src={title} alt="title"></img>
             <img className="menu__logo" src={logo} alt="logo"></img>
             <div className="menu__controllers">
-                <Link to="/records"><Button text="RECORDS" types={["menu", "secondary"]}/></Link>
-                <Link to="/game/settings"><Button text="NEW GAME" types={["menu"]} /></Link>
+                <Link to="./records" ><Button text="RECORDS" types={["menu", "secondary"]}/></Link>
+                <Link to="./game/settings" ><Button text="NEW GAME" types={["menu"]} /></Link>
             </div>
         </div>
     )
