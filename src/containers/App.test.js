ab1869a65ca0de66f26ec7c274209afea444d3fa
Gabriel García Rodríguez <ggarciaro@minsait.com>
2020-08-07 11:18:30 +0200

    Add homepage tests

---
+++ src/containers/App.test.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import App from './App'
+import { MemoryRouter } from 'react-router-dom'
+
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+
+describe('HomePage', () => {
+
+    beforeEach(() => {
+        render(
+            <MemoryRouter initialEntries={["/"]}>
+                <App />
+            </MemoryRouter>
+        )
+    })
+
+    afterEach(() => {
+        window.location.href= 'http://localhost/'
+    })
+
+    it('should redirect to records and show loading spinner if "RECORDS" button is pressed', () => {
+        const link = screen.getByRole('link', {name: /records/i})
+        userEvent.click(link)
+        const spinner = screen.getByRole('img', {name: /loading/i})
+        expect(spinner).toBeInTheDocument()
+    })
+    
+    it('should redirect to settings if "NEW GAME" button is pressed', () => {
+        const link = screen.getByRole('link', {name: /new game/i})
+        userEvent.click(link)
+        const decreaseBtn = screen.getByRole('img', {name: /decrease quantity/i})
+        expect(decreaseBtn).toBeInTheDocument()
+    })
+    
+})
+
+describe('tttt', () => {
+    
+})
